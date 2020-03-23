016f76d44f4cb88157e15fe7da3f5de1e014384d
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-03-23 14:06:40 -0300

    aula 04 - 02 - Selecionando as vendas do banco de dados

--- livraria/src/br/com/caelum/livraria/bean/VendasBean.java
+++ livraria/src/br/com/caelum/livraria/bean/VendasBean.java
@@ -1,19 +1,17 @@
 package br.com.caelum.livraria.bean;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
 
 import org.primefaces.model.chart.BarChartModel;
 import org.primefaces.model.chart.ChartSeries;
 
 import br.com.caelum.livraria.dao.LivroDao;
-import br.com.caelum.livraria.modelo.Livro;
 import br.com.caelum.livraria.modelo.Venda;
 
 @Named
@@ -23,7 +21,7 @@ public class VendasBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Inject
-	private LivroDao livroDao;
+	EntityManager manager;
 
 	public BarChartModel getVendasModel() {
 
@@ -33,38 +31,18 @@ public class VendasBean implements Serializable {
 		ChartSeries vendaSerie = new ChartSeries();
 		vendaSerie.setLabel("Vendas 2016");
 
-		List<Venda> vendas = getVendas(123);
+		List<Venda> vendas = getVendas();
 
 		for (Venda venda : vendas) {
 			vendaSerie.set(venda.getLivro().getTitulo(), venda.getQuantidade());
 		}
 
-		ChartSeries vendaSeries2015 = new ChartSeries();
-		vendaSeries2015.setLabel("Vendas 2015");
-
-		vendas = getVendas(12334);
-
-		for (Venda venda : vendas) {
-			vendaSeries2015.set(venda.getLivro().getTitulo(), venda.getQuantidade());
-		}
-
 		model.addSeries(vendaSerie);
-		model.addSeries(vendaSeries2015);
 		return model;
 	}
 
-	public List<Venda> getVendas(long seed) {
-
-		List<Livro> livros = livroDao.listaTodos();
-		List<Venda> vendas = new ArrayList<Venda>();
-
-		Random random = new Random(seed);
-
-		for (Livro livro : livros) {
-			Integer quantidade = random.nextInt(500);
-			vendas.add(new Venda(livro, quantidade));
-		}
-
+	public List<Venda> getVendas() {
+		List<Venda> vendas = this.manager.createQuery("select v from Venda v", Venda.class).getResultList();
 		return vendas;
 	}
 }
--- livraria/src/br/com/caelum/livraria/modelo/Venda.java
+++ livraria/src/br/com/caelum/livraria/modelo/Venda.java
@@ -1,9 +1,28 @@
 package br.com.caelum.livraria.modelo;
 
-public class Venda {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Venda implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@ManyToOne
 	private Livro livro;
 	private Integer quantidade;
+
+	public Venda() {
+	}
 
 	public Venda(Livro livro, Integer quantidade) {
 		this.livro = livro;
